b5dbfff1885d2640f01808c0ca13e22b16ed96aa
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2012-10-11 23:22:01 +0700

    Convert console program into a threaded server

--- bridge_of_death.py
+++ bridge_of_death.py
@@ -1,19 +1,40 @@
-import readline
+import socket
+import threading
+import SocketServer
 
-print 'Stop. Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.'
+class ThreadedTCPRequestHandler(SocketServer.StreamRequestHandler):
 
-print 'What... is your name?'
-name = raw_input()
+    def handle(self):
+        self.wfile.write("Stop.\nWho would cross the Bridge of Death must answer me\nthese questions three, ere the other side he see.\n")
 
-print 'What... is your quest?'
-quest = raw_input()
+        self.wfile.write("What... is your name?\n")
+        name = self.rfile.readline().strip()
 
-if 'arthur' in name.lower():
-	print 'What... is the air-speed velocity of an unladen swallow?'
-elif 'robin' in name.lower():
-	print 'What... is the capital of Assyria?'
-else:
-	print 'What... is your favourite colour?'
-last_answer = raw_input()
+        self.wfile.write("What... is your quest?\n")
+        quest = self.rfile.readline().strip()
 
-print 'Go on. Off you go.'
+        if "arthur" in name.lower():
+            self.wfile.write("What... is the air-speed velocity of an unladen swallow?\n")
+        elif "robin" in name.lower():
+            self.wfile.write("What... is the capital of Assyria?\n")
+        else:
+            self.wfile.write("What... is your favourite colour?\n")
+        last_answer = self.rfile.readline().strip()
+
+        self.wfile.write("Alright. Off you go.\n")
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 1975
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Don't exit the server thread when the main thread terminates
+    server_thread.daemon = False
+    server_thread.start()
