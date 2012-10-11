818627c8fdcf00fcaa94cf68b212f98ebac2b61f
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2012-10-11 18:07:02 +0700

    Initial implementation

---
+++ bridge_of_death.py
@@ -0,0 +1,17 @@
+import readline
+
+print 'Stop. Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.'
+
+print 'What... is your name?'
+name = raw_input()
+
+print 'What... is your quest?'
+quest = raw_input()
+
+if 'arthur' in name.lower():
+	print 'What... is the air-speed velocity of an unladen swallow?'
+elif 'robin' in name.lower():
+	print 'What... is the capital of Assyria?'
+else:
+	print 'What... is your favorite color?'
+last_answer = raw_input()
